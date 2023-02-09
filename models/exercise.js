207a617581c11360f03ac2d44e4498965ef850a6
TheodosisKats <theodosis.kats@gmail.com>
2023-02-09 22:03:24 +0200

    init commit

---
+++ models/exercise.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const exerciseSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: [true, "Must include a userId"]
+    },
+    description: {
+        type: String,
+        required: [true, "Must include a description"]
+    },
+    duration: {
+        type: Number,
+        required: [true, "Must include a duration"]
+    },
+    date: {
+        type: String
+    }
+})
+
+module.exports = mongoose.model('Exercise', exerciseSchema);
+
